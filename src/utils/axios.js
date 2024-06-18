9bad46640778edd4792e19e76fae81a3a989322b
Sanidhya Sharma <sanidhya626@gmail.com>
2024-06-18 12:43:16 +0530

    axios base url changed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ares.adaptable.app",
+   // baseURL: "https://ares.adaptable.app",
+   baseURL:"https://ares-backend-main.onrender.com"
 });
 
 export default instance;
